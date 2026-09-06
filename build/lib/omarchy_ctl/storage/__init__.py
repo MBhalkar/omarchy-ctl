dea792f3c1781eced2dccc85cf0ec563e09c737f
MB <maulik.bhalkar@gmail.com>
2026-09-06 23:13:38 +0530

    Remove committed build artifacts and ignore generated files

--- build/lib/omarchy_ctl/storage/__init__.py
+++
@@ -1,4 +0,0 @@
-from omarchy_ctl.storage.crypto import CryptoService
-from omarchy_ctl.storage.database import Database, get_storage
-
-__all__ = ["CryptoService", "Database", "get_storage"]
--- build/lib/omarchy_ctl/ui/__init__.py
+++
@@ -1,4 +0,0 @@
-from omarchy_ctl.ui.ipc import IPCService
-from omarchy_ctl.ui.quickshell import QuickshellWidget
-
-__all__ = ["IPCService", "QuickshellWidget"]
